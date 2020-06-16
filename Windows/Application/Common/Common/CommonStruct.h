962452ae02b19f829b5c41d364cb799921cff2ab
岳翔 <fly464447966@126.com>
2020-06-16 23:41:45 +0800

    添加COMMON_HOOK_INFO

--- Windows/Application/Common/Common/CommonStruct.h
+++ Windows/Application/Common/Common/CommonStruct.h
@@ -5,3 +5,17 @@
 
 // #include "CommonNickname.h"
 // #include "CommonNtDefine.h"
+
+typedef struct _COMMON_HOOK_INFO
+{
+    LPSTR lpProcName;
+    PVOID pNewAddress;
+    PVOID * ppOldAddress;
+
+    _COMMON_HOOK_INFO()
+    {
+        lpProcName = NULL;
+        pNewAddress = NULL;
+        ppOldAddress = NULL;
+    }
+} COMMON_HOOK_INFO;
