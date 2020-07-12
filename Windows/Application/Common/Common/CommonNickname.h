22b54740d1fa18af5b473a72e053201a5beb848b
岳翔 <fly464447966@126.com>
2020-07-12 22:17:35 +0800

    添加ExitProcess

--- Windows/Application/Common/Common/CommonNickname.h
+++ Windows/Application/Common/Common/CommonNickname.h
@@ -9,3 +9,15 @@
 typedef boost::shared_mutex CommonMutex;
 typedef boost::shared_lock<CommonMutex> CommonReadLock;
 typedef boost::unique_lock<CommonMutex> CommonWriteLock;
+
+typedef
+    VOID
+    (WINAPI * Kernel32_dll_ExitProcess_Proc_Type)(
+    __in UINT uExitCode
+    );
+
+typedef
+    VOID
+    (WINAPI * Kernelbase_dll_ExitProcess_Proc_Type)(
+    __in UINT uExitCode
+    );
